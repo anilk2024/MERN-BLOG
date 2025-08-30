6e6f79cf05a1dd19ce181e41a3c095d7059eb541
Anil Yadav <Anil.Yadav@smartenergywater.in>
2025-08-30 18:09:20 +0530

    created signup api route

---
+++ api/controllers/auth.controller.js
@@ -0,0 +1,34 @@
+import User from "../models/user.model.js";
+import bcrypt from "bcryptjs";
+
+export const signup = async (req, res) => {
+  const { username, email, password } = req.body;
+  if (
+    !username ||
+    !email ||
+    !password ||
+    username === "" ||
+    email === "" ||
+    password === ""
+  ) {
+    return res.status(400).json({ message: "All fields are required." });
+  }
+  const hashedPassword = bcrypt.hashSync(password, 10);
+  const newUser = new User({
+    username: username,
+    email: email,
+    password: hashedPassword,
+  });
+  try {
+    await newUser.save();
+    res.json({
+      status: 201,
+      message: "User craeted successfully..",
+    });
+  } catch (error) {
+    res.json({
+      status: 400,
+      messasage: error.message,
+    });
+  }
+};
