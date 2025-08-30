62c2a8756f91ee7130a9e5e006655fddcfb53cb7
Anil Yadav <Anil.Yadav@smartenergywater.in>
2025-08-30 12:04:38 +0530

    created test api route

--- api/index.js
+++ api/index.js
@@ -1,18 +1,22 @@
 import express from "express";
 import mongoose from "mongoose";
-import dotenv from 'dotenv';
+import dotenv from "dotenv";
+import userRoutes from "./routes/user.route.js";
 
 dotenv.config();
 
-
-mongoose.connect(process.env.MONGOCONN)
-.then(()=>{
-    console.log('MongoDb is connected');
-}).catch((err)=>{
+mongoose
+  .connect(process.env.MONGOCONN)
+  .then(() => {
+    console.log("MongoDb is connected");
+  })
+  .catch((err) => {
     console.log(err);
-})
+  });
 const app = express();
 
-app.listen(3000 ,()=>{
-    console.log('Server is running ate port 3000')
+app.listen(3000, () => {
+  console.log("Server is running at port 3000");
 });
+
+app.use("/api/user", userRoutes);
